a74eece871b48e498b6972d97dd3b8720ae3a6c2
juweyriyo <125019462+juweyriyo@users.noreply.github.com>
2024-09-17 18:52:41 +0300

    Update Schema New field added for the student's

--- backend/models/studentSchema.js
+++ backend/models/studentSchema.js
@@ -1,7 +1,17 @@
-const mongoose = require('mongoose');
-
 const studentSchema = new mongoose.Schema({
-    name: {
+    fullName: {
+        type: String,
+        required: true
+    },
+    studentTel: { // New field added for the student's studentTel
+        type: String,
+        required: true
+    },
+    parentName: { // New field added for the student's parentName
+        type: String,
+        required: true
+    },
+    parentTel: { // New field added for the student's parentTel
         type: String,
         required: true
     },
@@ -54,7 +64,28 @@ const studentSchema = new mongoose.Schema({
             ref: 'subject',
             required: true
         }
-    }]
-});
+    }],
+    transferStatus: { // New field added for the student's transferStatus
+        type: String,
+        enum: ['New', 'Transfer'],
+        required: true
+    },
+    previousSchoolName: { // New field added for the student's  previousSchoolName
+        type: String,
+        required: function() { return this.transferStatus === 'Transfer'; }
+    },
+    dateOfBirth: { // New field added for the student's dateOfBirth
+        type: Date,
+        required: true
+    },
+    placeOfBirth: { // New field added for the student's placeOfBirth
+        type: String,
+        required: true
+    },
+    currentAddress: {  // New field added for the student's current address
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
 
 module.exports = mongoose.model("student", studentSchema);
